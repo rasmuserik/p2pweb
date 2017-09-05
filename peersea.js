e497db765040feb0f72d2caa0aac19968efc1392
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-09-05 06:21:00 +0200

    start websocket isomorphic connections

--- peersea.js
+++ peersea.js
@@ -1,4 +1,4 @@
-// # PeerSea 
+// # PeerSea
 //
 // (literate source code below)
 //
@@ -21,31 +21,106 @@ wss://sea.solsort.com/
       window.location.hash &&
       window.location.hash.includes("SEA_BOOTSTRAP=") &&
       decodeURIComponent(
-        window.location.hash.replace(/.*SEA_BOOTSTRAP=/).replace(/[&].*/)
+        window.location.hash
+          .replace(/.*SEA_BOOTSTRAP=/, "")
+          .replace(/[&].*/, "")
       )) ||
     default_bootstrap)
     .trim()
-    .split(/\W+/);
+    .split(/\s+/);
 
-  /*
-const WebSocket = require('ws');
+  console.log("bootstrap nodes:", bootstrap_nodes);
 
-const port = process.env.SEA_PORT || 3535;
-const url = process.env.SEA_URL || 'http://localhost:' + port;
+  const assert = is_nodejs
+    ? require("assert")
+    : (ok, msg) => {
+        if (!ok) throw new Error("assert: " + msg);
+      };
 
-const wss = new WebSocket.Server({ port: 3535});
+  // ## Network
+  //
+  // - connection:
+  //     - onmessage `{data: ...}`
+  //     - send
+  //     - onclose
+  //     - close
+  // - `start_signalling(signalling_connection)`
+  // - `receive_signalling(signalling_connection)`
+  // - callback `connected(connection)`
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+  function connected(con) {
+    con.onmessage = msg => console.log("msg", msg);
+    con.onclose = () => console.log("close", con);
+    con.send(`hello ${is_nodejs}`);
+  }
+  let start_signalling;
+  let receive_signalling;
 
-  ws.send('something');
-});
-const bootstrap_nodes = self.SEA_BOOTSTRAP || [];
-console.log(bootstrap_nodes);
-*/
+  setTimeout(() => {
+    const o = { close: () => {} };
+    let con = receive_signalling(o);
+    o.onmessage({
+      data: JSON.stringify({ websocket: bootstrap_nodes[0] })
+    });
+  }, 1000 * Math.random());
 
-  // Hello
+  // ### NodeJS
+  //
+  if (is_nodejs) {
+    const WebSocket = require("ws");
 
+    const port = process.env.SEA_PORT;
+    assert(port);
+
+    const url = process.env.SEA_URL;
+    assert(url);
+
+    const wss = new WebSocket.Server({ port: port });
+
+    wss.on("connection", function connection(ws) {
+      const con = {
+        send: msg => ws.send(msg),
+        close: () => ws.close()
+      };
+      connected(con);
+      ws.on(
+        "message",
+        msg => con.onmessage && con.onmessage({ data: msg })
+      );
+      ws.on("close", () => con.onclose && con.onclose());
+    });
+    receive_signalling = o => {
+      o.onmessage = () => {};
+      return {};
+    };
+  }
+
+  // ### Browser
+  //
+  if (!is_nodejs) {
+    receive_signalling = signal_connection => {
+      signal_connection.onmessage = signal_message => {
+        signal_message = JSON.parse(signal_message.data);
+        if (signal_message.websocket) {
+          const con = {};
+          const ws = new WebSocket(signal_message.websocket);
+          con.send = msg => ws.send(msg);
+          con.close = () => ws.close();
+          ws.onmessage = msg => con.onmessage && con.onmessage(msg);
+          ws.onerror = err => {
+            con.onclose && con.onclose();
+            signal_connection.close();
+          }
+          ws.onclose = () => con.onclose && con.onclose();
+          ws.onopen = () => {
+            connected(con);
+            signal_connection.close();
+          };
+        } else {
+          signal_connection.close();
+          throw "WebRTC not implemented yet";
+        }
+      };
+    };
+  }
 })();

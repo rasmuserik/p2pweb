bf16ede88a664e6894b17dd4344181a7b460ed8e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-09-09 05:26:37 +0200

    name fixes

--- dev.sh
+++ dev.sh
@@ -17,22 +17,22 @@ google-chrome \
   "http://localhost:8080/#SEA_BOOTSTRAP=ws://localhost:3500" \
   "http://localhost:8080/#SEA_BOOTSTRAP=ws://localhost:3501" \
   &
-touch peersea.js) &
-while inotifywait -e modify,close_write,move_self -q peersea.js
+touch p2pweb.js) &
+while inotifywait -e modify,close_write,move_self -q p2pweb.js
 do 
   kill `cat .pid`
   sleep 0.1
   SEA_URL=ws://localhost:3500 \
   SEA_BOOTSTRAP=ws://localhost:3501 \
   SEA_PORT=3500 \
-  node peersea.js &
+  node p2pweb.js &
   echo $! > .pid
   sleep 1;
 
   SEA_URL=ws://localhost:3501 \
   SEA_BOOTSTRAP=ws:localhost:3500 \
   SEA_PORT=3501 \
-  node peersea.js &
+  node p2pweb.js &
   echo $! >> .pid
   sleep 1
 done

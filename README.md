9688223ca3cd91dfd18c7f762f859c6178ff1cf9
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-09-09 07:35:05 +0200

    travis

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
+#
+
+<img src=https://forsider.solsort.com/icon.png width=96 height=96 align=right>
+
+[![website](https://img.shields.io/badge/website-p2pweb.solsort.com-blue.svg)](https://p2pweb.solsort.com/)
+[![github](https://img.shields.io/badge/github-solsort/p2pweb-blue.svg)](https://github.com/solsort/p2pweb)
+[![travis](https://img.shields.io/travis/solsort/p2pweb.svg)](https://travis-ci.org/solsort/p2pweb)
+[![npm](https://img.shields.io/npm/v/p2pweb.svg)](https://www.npmjs.com/package/p2pweb)
+
 # P2P Web
+
+
 
 This is a library for building peer-to-peer web applications.
 
--- p2pweb.js
+++ p2pweb.js
@@ -1,8 +1,8 @@
-// # 
+// #
 //
 // <img src=https://forsider.solsort.com/icon.png width=96 height=96 align=right>
 //
-// [![website](https://img.shields.io/badge/website-p2pweb.solsort.com-blue.svg)](https://p2pweb.solsort.com/) 
+// [![website](https://img.shields.io/badge/website-p2pweb.solsort.com-blue.svg)](https://p2pweb.solsort.com/)
 // [![github](https://img.shields.io/badge/github-solsort/p2pweb-blue.svg)](https://github.com/solsort/p2pweb)
 // [![travis](https://img.shields.io/travis/solsort/p2pweb.svg)](https://travis-ci.org/solsort/p2pweb)
 // [![npm](https://img.shields.io/npm/v/p2pweb.svg)](https://www.npmjs.com/package/p2pweb)

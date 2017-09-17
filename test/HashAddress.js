3873d72c955e673a639279bfdbd9db92d85c4f09
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-09-17 14:53:53 +0200

    use assert instead of throwError

--- test/HashAddress.js
+++ test/HashAddress.js
@@ -1,12 +1,13 @@
 const HashAddress = require("../src/HashAddress");
+const assert = require("assert");
 
 describe("HashAddress", () => {
   it("can create addresses, and they support equality check", async () => {
     let a = await HashAddress.generate("hello world");
     let b = await HashAddress.generate("hello world");
     let c = await HashAddress.generate("hello wørld");
-    a.equals(b) || throwError("equals1");
-    !a.equals(c) || throwError("equals2");
+    assert.equal(a.equals(b), true);
+    assert.equal(a.equals(c), false);
   });
 
   it("converts to/from ArrayBuffer/String", async () => {
@@ -16,9 +17,9 @@ describe("HashAddress", () => {
     let x80 = HashAddress.fromString(
       "gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     );
-    a.equals(b) || throwError();
-    a.equals(c) || throwError();
-    x80.toHex().startsWith("800") || throwError();
+    assert(a.equals(b));
+    assert(a.equals(c));
+    assert(x80.toHex().startsWith("800"));
   });
 
   it("measures xor-distance to other HashAddresses", () => {
@@ -30,24 +31,24 @@ describe("HashAddress", () => {
     h = HashAddress.fromHex(
       "0000000000000000000000000000001000000000000000000000000000000000"
     );
-    zero.dist(h) === 1 || throwError();
+    assert.equal(zero.dist(h), 1);
 
     h = HashAddress.fromHex(
       "8000000000000000000000000000000000000000000000000000000000000000"
     );
-    zero.dist(h) === 2 ** 123 || throwError();
-    zero.distBit(h) === 0 || throwError();
+    assert.equal(zero.dist(h), 2 ** 123);
+    assert.equal(zero.distBit(h), 0);
 
     h = HashAddress.fromHex(
       "0000000000000000000000000000000000000000000000000000000000000001"
     );
-    zero.dist(h) === 2 ** -132 || throwError();
-    zero.distBit(h) === 255 || throwError();
+    assert.equal(zero.dist(h), 2 ** -132);
+    assert.equal(zero.distBit(h), 255);
 
     h = HashAddress.fromHex(
       "0f00000000000000000000000000000000000000000000000000000000000000"
     );
-    zero.distBit(h) === 4 || throwError();
+    assert.equal(zero.distBit(h), 4);
   });
 
   it("can flip a given bit, and randomize thereadter", () => {
@@ -55,17 +56,23 @@ describe("HashAddress", () => {
       "0000000000000000000000000000000000000000000000000000000000000000"
     );
 
-    zero
-      .flipBitRandomise(3)
-      .toHex()
-      .startsWith("1") || throwError();
-    zero
-      .flipBitRandomise(7)
-      .toHex()
-      .startsWith("01") || throwError();
-    zero
-      .flipBitRandomise(7 + 8)
-      .toHex()
-      .startsWith("0001") || throwError();
+    assert(
+      zero
+        .flipBitRandomise(3)
+        .toHex()
+        .startsWith("1")
+    );
+    assert(
+      zero
+        .flipBitRandomise(7)
+        .toHex()
+        .startsWith("01")
+    );
+    assert(
+      zero
+        .flipBitRandomise(7 + 8)
+        .toHex()
+        .startsWith("0001")
+    );
   });
 });
